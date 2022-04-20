964029ecb7bfd11e64a4ac3442db510a4bfb9ace
alishajain <jain.alisha14@gmail.com>
2022-04-20 12:04:52 +0530

    Edit and delete buttons working

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,13 +1,28 @@
 //Todo App
-let task = document.querySelector("#new_task");
+let task = document.getElementById("new_task");
 let addBtn = document.querySelector("#add");
 let output = document.getElementById("task_list");
 
-function getResult(){
-    console.log(task.value);
-    output.innerHTML += task.value;
+function delTask(){
+    console.log("task deleted");
+}
 
-    let button = document.getElementById("buttons");
+function editTask(){
+    console.log("task edited");
+}
+
+function getResult(){
+    let li = document.createElement("li");
+    let inputValue = task.value;
+    let text = document.createTextNode(inputValue);
+    
+    li.appendChild(text);
+    if(inputValue === "") {
+        alert("Please write a task");
+    } else {
+        document.getElementById("task_list").appendChild(li);
+    }
+    task.value = '';
 
     let edit_btn = document.createElement("button");
     let del_btn = document.createElement("button");
@@ -15,11 +30,17 @@ function getResult(){
     let edit = document.createTextNode("Edit");
     let del = document.createTextNode("Delete");
 
+    edit_btn.className = "edit";
+    del_btn.className = "close";
+
     edit_btn.appendChild(edit);
     del_btn.appendChild(del);
 
-    button.appendChild(edit_btn);
-    button.appendChild(del_btn);
+    li.appendChild(edit_btn);
+    li.appendChild(del_btn);
+
+    del_btn.addEventListener("click", delTask);
+    edit_btn.addEventListener("click", editTask);
 }
 
 addBtn.addEventListener("click", getResult);
