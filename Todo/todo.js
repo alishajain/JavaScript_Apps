503835b6e4ea9f72fa8ebd3f4ccd7b3728c1d1b6
alishajain <jain.alisha14@gmail.com>
2022-04-20 13:07:17 +0530

    Edit button not working properly

--- Todo/todo.js
+++ Todo/todo.js
@@ -2,16 +2,35 @@
 let task = document.getElementById("new_task");
 let addBtn = document.querySelector("#add");
 let output = document.getElementById("task_list");
-
-function delTask(){
+let addBtn_value = "add";
+function delTask(e){
     console.log("task deleted");
+    if(e.target.classList.contains("close")){
+        let li = e.target.parentNode;
+        task_list.removeChild(li);
+    }
 }
 
-function editTask(){
+function editTask(e){
     console.log("task edited");
+    addBtn_value = "edit";
+
+    if(e.target.classList.contains("edit")) {
+        let edited = e.target.parentNode.childNodes[0].data;
+        task.value = edited;
+        if(addBtn_value === "edit"){
+        addBtn.onclick = function change(){
+        e.target.parentNode.childNodes[0].data = task.value;
+        task.value = '';
+        addBtn_value = "add";
+        }
+        } 
+    }
 }
 
 function getResult(){
+    if(addBtn_value === "add"){
+
     let li = document.createElement("li");
     let inputValue = task.value;
     let text = document.createTextNode(inputValue);
@@ -32,7 +51,7 @@ function getResult(){
 
     edit_btn.className = "edit";
     del_btn.className = "close";
-
+    
     edit_btn.appendChild(edit);
     del_btn.appendChild(del);
 
@@ -41,6 +60,7 @@ function getResult(){
 
     del_btn.addEventListener("click", delTask);
     edit_btn.addEventListener("click", editTask);
+}
 }
 
 addBtn.addEventListener("click", getResult);
