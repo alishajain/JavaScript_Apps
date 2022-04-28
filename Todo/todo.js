0e6b921fa57e96008069728e5fc6a0e015b04b82
alishajain <jain.alisha14@gmail.com>
2022-04-28 11:40:13 +0530

    fix:edit button bug

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,6 +1,7 @@
 //Todo App
 let task = document.getElementById("new_task");
-let addBtn = document.querySelector("#add");
+const addBtn = document.querySelector("#add");
+const editBtn = document.querySelector("#edit");
 let output = document.getElementById("task_list");
 let list = document.querySelector('ul');
 let addBtn_value = "add";
@@ -26,12 +27,16 @@ function editTask(e){
 
     if(e.target.classList.contains("edit")) {
         let edited = e.target.parentNode.childNodes[0].data;
+        editBtn.style.display = "inline-block";
+        addBtn.style.display = "none";
         task.value = edited;
         if(addBtn_value === "edit"){
-            addBtn.onclick = function change(){
+            editBtn.onclick = function change(){
                 e.target.parentNode.childNodes[0].data = task.value;
                 addBtn_value = 'add';
                 task.value = '';
+                editBtn.style.display="none";
+                addBtn.style.display="inline-block";
             }
         }
     } 
@@ -49,27 +54,26 @@ function getResult(){
     if(inputValue === "") {
         alert("Please write a task");
     } else {
+        
+        let edit_btn = document.createElement("button");
+        let del_btn = document.createElement("button");
+
+        let edit = document.createTextNode("Edit");
+        let del = document.createTextNode("Delete");
+
+        edit_btn.className = "edit";
+        del_btn.className = "close";
+    
+        edit_btn.appendChild(edit);
+        del_btn.appendChild(del);
+    
+        li.appendChild(edit_btn);
+        li.appendChild(del_btn);
         document.getElementById("task_list").appendChild(li);
+        edit_btn.addEventListener("click", editTask);
+        del_btn.addEventListener("click", delTask);
     }
     task.value = '';
-
-    let edit_btn = document.createElement("button");
-    let del_btn = document.createElement("button");
-
-    let edit = document.createTextNode("Edit");
-    let del = document.createTextNode("Delete");
-
-    edit_btn.className = "edit";
-    del_btn.className = "close";
-    
-    edit_btn.appendChild(edit);
-    del_btn.appendChild(del);
-    
-    li.appendChild(edit_btn);
-    li.appendChild(del_btn);
-
-    edit_btn.addEventListener("click", editTask);
-    del_btn.addEventListener("click", delTask);
 }
 }
 
